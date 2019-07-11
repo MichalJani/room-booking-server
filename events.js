800f83345ed4480e19daf8b0e4105acc91f0278e
Michal Janicki <michal.janicki1@gmail.com>
2019-07-11 23:47:25 +0100

    Add PUT and DELETE routes

--- events.js
+++ events.js
@@ -74,7 +74,9 @@ function getAccessToken(oAuth2Client, callback) {
   });
 }
 
-
+// @route    GET api/events
+// @desc     Get all events
+// @access   Private
 router.get('/', (req, res) => {
   readCredentials(listEvents);
 
@@ -88,18 +90,25 @@ router.get('/', (req, res) => {
       orderBy: 'startTime',
     }, (err, res) => {
 
-      if (err) return console.log('The API returned an error: ' + err);
-      const events = res.data.items;
-      respond(events);
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        respondError(err);
+        return;
+      }
+      respond(res);
     })
   }
-
-  function respond(events) {
-    res.status(200).json(events)
+  function respondError(calError) {
+    res.status(calError.code).json(calError.errors[0]);
+  }
+  function respond(calRes) {
+    res.status(calRes.status).json(calRes.data.items);
   }
 })
 
-
+// @route    POST api/events
+// @desc     Add new event
+// @access   Private
 router.post('/', (req, res) => {
   readCredentials(insertEvent);
   function insertEvent(auth) {
@@ -111,17 +120,104 @@ router.post('/', (req, res) => {
     }, function (err, res) {
       if (err) {
         console.log('There was an error contacting the Calendar service: ' + err);
+        respondError(err)
         return;
       }
-      const event = res.data;
-      respond(event);
+      respond(res);
     })
   }
-  function respond(event) {
-    res.status(200).json(event);
+  function respondError(calError) {
+    res.status(calError.code).json(calError.errors[0]);
+  }
+  function respond(calRes) {
+    res.status(calRes.status).json(calRes.data);
   }
 })
 
+// @route    GET api/events/:id
+// @desc     Get event by id
+// @access   Private
+router.get('/:id', (req, res) => {
+  readCredentials(getEvent);
+  function getEvent(auth) {
+    const calendar = google.calendar({ version: 'v3', auth });
+    calendar.events.get({
+      auth: auth,
+      calendarId: 'primary',
+      eventId: req.params.id,
+    }, function (err, res) {
+      if (err) {
+        console.log('There was an error contacting the Calendar service: ' + err);
+        respondError(err)
+        return;
+      }
+      respond(res);
+    })
+  }
+  function respondError(calError) {
+    res.status(calError.code).json(calError.errors[0]);
+  }
+  function respond(calRes) {
+    res.status(calRes.status).json(calRes.data);
+  }
+})
+
+// @route    DELETE api/events/:id
+// @desc     Delete event by id
+// @access   Private
+router.delete('/:id', (req, res) => {
+  readCredentials(deleteEvent);
+  function deleteEvent(auth) {
+    const calendar = google.calendar({ version: 'v3', auth });
+    calendar.events.delete({
+      auth: auth,
+      calendarId: 'primary',
+      eventId: req.params.id,
+    }, function (err, res) {
+      if (err) {
+        console.log('There was an error contacting the Calendar service: ' + err);
+        respondError(err)
+        return;
+      }
+      respond(res);
+    })
+  }
+  function respondError(calError) {
+    res.status(calError.code).json(calError.errors[0]);
+  }
+  function respond(calRes) {
+    res.status(calRes.status).send({ message: 'Event has been deleted.' });
+  }
+})
+
+// @route    PUT api/events
+// @desc     Update event
+// @access   Private
+router.put('/:id', (req, res) => {
+  readCredentials(updateEvent);
+  function updateEvent(auth) {
+    const calendar = google.calendar({ version: 'v3', auth });
+    calendar.events.update({
+      auth: auth,
+      calendarId: 'primary',
+      eventId: req.params.id,
+      resource: req.body,
+    }, function (err, res) {
+      if (err) {
+        console.log('There was an error contacting the Calendar service: ' + err);
+        respondError(err)
+        return;
+      }
+      respond(res);
+    })
+  }
+  function respondError(calError) {
+    res.status(calError.code).json(calError.errors[0]);
+  }
+  function respond(calRes) {
+    res.status(calRes.status).json(calRes.data);
+  }
+})
 
 
 module.exports = router;
