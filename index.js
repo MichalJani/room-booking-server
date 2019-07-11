858fda2b73af1b42d2fdf3a038f99c90062d9cca
Michal Janicki <michal.janicki1@gmail.com>
2019-07-11 23:55:22 +0100

    Comment cal api

--- index.js
+++ index.js
@@ -7,12 +7,8 @@ const express = require('express');
 const app = express();
 app.use(express.json({ extended: false }));
 
-app.use('/api/calendars', require('./calendars'));
+// app.use('/api/calendars', require('./calendars'));
 app.use('/api/events', require('./events'));
-
-
-
-
 
 const PORT = process.env.PORT || 5000;
 
