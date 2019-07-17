3ab191c4d360ff8f4a8be2cd32ae804c29f9ce86
Michal Janicki <michal.janicki1@gmail.com>
2019-07-17 18:41:34 +0200

    Separate authentication to make it first time only

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const fs = require('fs');
-const readline = require('readline');
-const { google } = require('googleapis');
+// const fs = require('fs');
+// const readline = require('readline');
+// const { google } = require('googleapis');
 const express = require('express');
 // const path = require('path');
 
+
+
 const app = express();
 app.use(express.json({ extended: false }));
+const events = require('./events');
+
 
 // app.use('/api/calendars', require('./calendars'));
-app.use('/api/events', require('./events'));
+app.use('/api/events', events);
 
 const PORT = process.env.PORT || 5000;
 
